d57a4a83dc31173ed30d70a8bc24dfe774e2459c
Asadbek Akramov <asadbek1999.aaa@gmail.com>
2020-07-31 18:36:05 +0500

    commited by Mr.Akramov 18:35, 31.01.2020

--- app/src/main/java/uz/mobiler/adiblar/fragments/data/WriterInfoFragment.kt
+++ app/src/main/java/uz/mobiler/adiblar/fragments/data/WriterInfoFragment.kt
@@ -7,9 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.CollapsingToolbarLayout
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.like.LikeButton
+import com.like.OnLikeListener
+import kotlinx.android.synthetic.main.content_scrolling.view.*
 import kotlinx.android.synthetic.main.fragment_writer_info.view.*
+import kotlinx.android.synthetic.main.fragment_writer_info.view.like_back
+import kotlinx.android.synthetic.main.item_writer.view.*
 import uz.mobiler.adiblar.R
+import uz.mobiler.adiblar.database.MyDBHelper
 import uz.mobiler.adiblar.models.Writer
 
 
@@ -23,19 +30,50 @@ class WriterInfoFragment : Fragment() {
     ): View? {
         root = inflater.inflate(R.layout.fragment_writer_info, container, false)
 
+        val myDBHelper = MyDBHelper(root.context)
         if (arguments != null) {
             writer = arguments?.getSerializable("writer") as Writer
         }
 
         val toolbar: androidx.appcompat.widget.Toolbar = root.findViewById(R.id.toolbar)
-
         val activity = activity as AppCompatActivity?
         activity?.setSupportActionBar(toolbar)
 
         root.toolbar_layout.title = writer.writer
         root.toolbar_layout.setCollapsedTitleTextColor(Color.BLACK)
+        root.toolbar_layout.setExpandedTitleColor(Color.BLACK)
 
+        root.like_btn1.isLiked = myDBHelper.getWriterById(writer)
 
+        if (myDBHelper.getWriterById(writer)) {
+            root.like_back.setBackgroundResource(R.drawable.unlike_background)
+        } else {
+            root.like_back.setBackgroundResource(R.drawable.like_background)
+        }
+
+        root.like_btn1.setOnLikeListener(object : OnLikeListener {
+            override fun liked(likeButton: LikeButton?) {
+                myDBHelper.addWriter(writer)
+                root.like_back.setBackgroundResource(R.drawable.unlike_background)
+            }
+
+            override fun unLiked(likeButton: LikeButton?) {
+                myDBHelper.deleteWriter(writer)
+                root.like_back.setBackgroundResource(R.drawable.like_background_white)
+            }
+
+        })
+
+        Glide.with(root).load(writer.imgUrl).placeholder(R.drawable.place_holder)
+            .centerCrop().error(R.drawable.error_image).into(root.iv_image)
+
+        root.tv_info.text = writer.description
+
+        root.tv_birthDeath.text = ("(${writer.birthDead})")
+
+        root.btn_back.setOnClickListener {
+            findNavController().popBackStack()
+        }
         return root
     }
 
