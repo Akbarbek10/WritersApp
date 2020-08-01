32faf9dcb7c20901351cf576d57d1dda35ce95c9
Sanjar Suvonov <sanjar.tatu.96@gmail.com>
2020-08-01 12:25:02 +0500

    add lotinkirilservice

---
+++ app/src/main/java/uz/mobiler/adiblar/utils/LotinKrilService.kt
@@ -0,0 +1,37 @@
+package uz.mobiler.adiblar.utils
+
+object LotinKrilService {
+
+    fun convert(k: String): String {
+        var k = k
+        val x = "ABSYUKEHGOZXFQVPRLDJEMITabsyukengozxfqvprldjemith"
+        val y = "АБСЙУКЕҲГОЗХФҚВПРЛДЖЭМИТабсйукенгозхфқвпрлджэмитҳ"
+        k = k.replace("yo", "ё")
+        k = k.replace("ch", "ч")
+        k = k.replace("sh", "ш")
+        k = k.replace("yu", "ю")
+        k = k.replace("ya", "я")
+        k = k.replace("Yo", "Ё")
+        k = k.replace("Ch", "Ч")
+        k = k.replace("Yu", "Ю")
+        k = k.replace("Ya", "Я")
+        k = k.replace("Sh", "Ш")
+        k = k.replace("O'", "Ў")
+        k = k.replace("O’", "Ў")
+        k = k.replace("o'", "ў")
+        k = k.replace("o’", "ў")
+        k = k.replace("g'", "ғ")
+        k = k.replace("G'", "Ғ")
+        k = k.replace("ye", "е")
+        k = k.replace("'", "ъ")
+        k = k.replace("’", "ъ")
+        val stringBuffer = StringBuilder(k)
+        for (i in k.indices) {
+            for (j in 0..48) if (stringBuffer[i] == x[j]) stringBuffer.setCharAt(
+                i,
+                y[j]
+            )
+        }
+        return stringBuffer.toString()
+    }
+}
